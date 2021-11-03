d8ad7a33de780809570e40afdcebf7b5198c541d
Aqil <aqilzeka99@gmail.com>
2021-11-03 16:45:53 +0400

    Create project and implementations of frontend side

---
+++ src/main/java/com/example/fleetmanagement/ApplicationController.java
@@ -0,0 +1,18 @@
+package com.example.fleetmanagement;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/widgets")
+    public String widgets() {
+        return "widgets";
+    }
+}
